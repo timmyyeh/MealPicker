c5f5bb7dfb75d82f607a8636a087d69162a524c0
timmy.yeh@docusign.com <timmyyeh@cs.washington.edu>
2020-12-21 00:00:30 -0800

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
 import Search from './components/Search';
-import './App.css';
+import Navbar from './components/CustomNavbar';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <h1>Home page</h1>
-        <Link to='/search'>Search</Link>
-        <Switch>
-          <Route path='/search'>
-            <Search />
-          </Route>
-        </Switch>
-      </div>
+        <Navbar />
+        <Container>
+          <Link to='/search'>Search</Link>
+          <Switch>
+            <Route path='/search'>
+              <Search />
+            </Route>
+          </Switch>
+        </Container>
     </Router>
   );
 }
