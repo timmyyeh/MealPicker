fba16c17a303ca8c21693524997bbf1af19e8b83
timmy.yeh@docusign.com <timmyyeh@cs.washington.edu>
2020-12-20 23:24:38 -0800

    router for react

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Search from './components/Search';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Home page</h1>
+        <Link to='/search'>Search</Link>
+        <Switch>
+          <Route path='/search'>
+            <Search />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
