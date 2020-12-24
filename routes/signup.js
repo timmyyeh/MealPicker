af57d427718b8702c911b00cc5d734bbf59456b6
timmy.yeh@docusign.com <timmyyeh@cs.washington.edu>
2020-12-24 01:48:12 -0800

    hash and validate password and account

--- routes/signup.js
+++ routes/signup.js
@@ -1,14 +1,29 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+const express = require("express")
+const router = express.Router()
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
 
-router.post('/add', async (req, res) => {
-    try {
-        console.log(req.body);
-    }
-    catch (error) {
-        
-    }
-});
+router.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body
+    validate(email, password)
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+})
 
-module.exports = router;
+function validate(email, password) {
+  if (!validator.isEmail(email)) {
+    throw "Is not a valid email"
+  }
+
+  if (!validator.isStrongPassword(password, { minNumbers: 0, minSymbols: 0 })) {
+    throw "Password is not strong enough"
+  }
+}
+
+module.exports = router
