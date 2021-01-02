8763e6584aca5c44bdeea44508d2c1b86eca7e55
timmy.yeh@docusign.com <timmyyeh@cs.washington.edu>
2021-01-01 17:53:07 -0800

    add search route

--- routes/search.js
+++ routes/search.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
-router.get('/healthcheck', (req, res) => {
-   res.send('health');
+router.get("/healthcheck", (req, res) => {
+  res.send("health");
 });
 
-router.get('/:postal', async (req, res) => {
-    try {
-        const response = await axios.get(`https://api.yelp.com/v3/businesses/search?location=\"Seattle\"`, {
-            headers: {'Authorization': `Bearer ${process.env.YELP_APIKEY}`}
-        });
-        console.log(response.data.businesses);
-        
-    } catch (err) {
-        console.log(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const { city, state, zipcode } = req.query;
+    console.log(`city: ${city} zip: ${zipcode}`);
+    const response = await axios.get(
+      `https://api.yelp.com/v3/businesses/search?location=\"${city}, ${state} ${zipcode}\"`,
+      {
+        headers: { Authorization: `Bearer ${process.env.YELP_APIKEY}` },
+      }
+    );
+    console.log(response.data.businesses);
+    res.json(response.data.businesses);
+  } catch (err) {
+    console.log(err);
+  }
 });
-
-
 
 module.exports = router;
