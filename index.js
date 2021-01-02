96580cbd6cd8f289b17a64e3b15a599cafd00958
timmy.yeh@docusign.com <timmyyeh@cs.washington.edu>
2021-01-01 17:12:40 -0800

    add login route

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require("express")
-const connectDB = require("./config/db.js")
-const app = express()
+const express = require("express");
+const connectDB = require("./config/db.js");
+const app = express();
 
 // init env
 require("dotenv").config({
   path: "./config/config.env",
-})
+});
 
 // connect to db
-connectDB()
+connectDB();
 
 // body parser
-app.use(express.json())
+app.use(express.json());
 
 // use route
-app.use("/search", require("./routes/search.js"))
-app.use("/login", require("./routes/login.js"))
-app.use("/signup", require("./routes/signup.js"))
+app.use("/search", require("./routes/search.js"));
+app.use("/login", require("./routes/login.js"));
+app.use("/signup", require("./routes/signup.js"));
 
 app.get("/healthcheck", (req, res) => {
-  res.send("healthy")
-})
+  res.send("healthy");
+});
 
-const PORT = process.env.PORT | 8080
+const PORT = process.env.PORT | 8080;
 
 app.listen(PORT, () => {
-  console.log(`App is running at ${PORT}`)
-})
+  console.log(`App is running at ${PORT}`);
+});
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,28 @@
-const express = require('express');
+const express = require("express");
+const Users = require("../models/Users");
+const bcrypt = require("bcrypt");
+
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Users.findOne({ email }).exec();
 
+    if (!user) {
+      res.send("user does not exist");
+    }
 
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      res.send("password does not match");
+    }
+
+    console.log(`user is authenticated`);
+    res.json(user);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 module.exports = router;
